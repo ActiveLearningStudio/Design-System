92c7de1da5aca00fc791efbe464c0e76be205753
aquib <aquib@curriki.org>
2021-09-22 18:52:24 +0500

    checkboxes added

--- src/lib/utils/Checkboxes/checkboxes.js
+++ src/lib/utils/Checkboxes/checkboxes.js
@@ -2,45 +2,34 @@
 import React from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
-import {Form} from 'react-bootstrap';
+import { Form } from "react-bootstrap";
 import "./checkboxes.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-
-const Checkboxes = ({
-  text,
-  type,
-  className,
-  primary,
-  secondary,
-  icon,
-  width,
-  height,
-  radius,
-  disabled,
- 
-  onClick = () => {},
-}) => {
+const Checkboxes = ({ label, className, disabled, onClick = () => {} }) => {
+  const disbaledCheckbox = classNames(
+    disabled && "curriki-disbaled-checkbox",
+    className
+  );
   return (
-    <Form.Group className="mb-3"  controlId="formBasicCheckbox">
-    <Form.Check className="curriki-default-checkbox" type="checkbox" label="Check me out" />
-  </Form.Group>
-    
+    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+      <Form.Check
+        disabled={disabled}
+        className={"curriki-default-checkbox " + disbaledCheckbox}
+        type="checkbox"
+        label={label && label}
+      />
+    </Form.Group>
   );
 };
 
 Checkboxes.propTypes = {
-  text: PropTypes.string,
-  type: PropTypes.string,
+  label: PropTypes.string,
   disabled: PropTypes.bool,
   className: PropTypes.string,
-  primary: PropTypes.bool,
-  secondary: PropTypes.bool,
-  icon: PropTypes.any,
-  width: PropTypes.string,
-  height: PropTypes.string,
-  radius: PropTypes.string,
+  // icon: PropTypes.any,
+  // // width: PropTypes.string,
+  // // height: PropTypes.string,
   onClick: PropTypes.func,
-
 };
 export default Checkboxes;
--- src/lib/utils/Checkboxes/checkboxes.stories.js
+++ src/lib/utils/Checkboxes/checkboxes.stories.js
@@ -6,9 +6,6 @@ import { faTimes } from "@fortawesome/free-solid-svg-icons";
 export default {
   title: "Utilities/Checkboxes",
   component: Checkboxes,
-  argTypes: {
-    backgroundColor: { control: "color" },
-  },
 };
 
 const Template = (args) => <Checkboxes {...args} />;
@@ -16,13 +13,6 @@ const Template = (args) => <Checkboxes {...args} />;
 export const CheckboxProps = Template.bind({});
 
 CheckboxProps.args = {
-  primary: true,
-  secondary: false,
   disabled: false,
-  icon: faTimes,
-  width: "",
-  height: "",
-  radius: "",
-  text: "text",
-  type: "submit",
+  label: "checkbox text",
 };
